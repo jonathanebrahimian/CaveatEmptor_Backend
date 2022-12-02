75a2c349bb6a2162966a87f32b83bfd868e05c12
jonathanebrahimian <jebrahimian@smu.edu>
2022-12-02 17:38:06 -0600

    Adding DB in

--- app/database.py
+++ app/database.py
@@ -1,15 +1,15 @@
-# import pymongo
-# import os
+import pymongo
+import os
 
-# MONGO_DETAILS = os.environ.get('MONGODB_URI_SRV')
+MONGO_DETAILS = os.environ.get('MONGODB_URI_SRV')
 
-# client = pymongo.MongoClient(MONGO_DETAILS)
+client = pymongo.MongoClient(MONGO_DETAILS)
 
-# database = client.caveat_emptor
+database = client.caveat_emptor
 
-# report_collection = database.get_collection("report")
-# report_collection.create_index([("address", pymongo.ASCENDING)], unique=True)
+report_collection = database.get_collection("report")
+report_collection.create_index([("address", pymongo.ASCENDING)], unique=True)
 
-# stats_collection = database.get_collection("stats")
-# algo_update_collection = database.get_collection("algo_update")
+stats_collection = database.get_collection("stats")
+algo_update_collection = database.get_collection("algo_update")
 
